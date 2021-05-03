ec416ebf7764cfdde7613efe77d18fd376db72b3
Liam Harbec <liam.harbec@gmail.com>
2021-05-03 11:39:45 -0700

    Added popper.js and added support for select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import $ from 'jquery';
+import Select2 from 'select2';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+
+require('jquery');
+require('select.js');
---
+++ app/javascript/select.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function() {
+    console.log("setting up select2");
+    
+});
